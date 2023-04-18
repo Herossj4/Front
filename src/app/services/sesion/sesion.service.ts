60870868c265c9e252a783334e29f89d51cc5002
Jeison <davidbuitrago1996@hotmail.com>
2023-04-18 00:36:54 -0500

    18/04/2023

--- src/app/services/sesion/sesion.service.ts
+++ src/app/services/sesion/sesion.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import * as CryptoJS from 'crypto-js';
 import { Entitys } from 'src/app/Entitys';
 
 declare var Swal:any;
@@ -13,6 +13,72 @@ export class AccesoService {
 
   constructor(private router : Router) {}
 
+  Encriptarllave(dato:any){
+    var tamaño = 256;
+    var enc = CryptoJS.lib.WordArray.random(16);
+    var Key = CryptoJS.PBKDF2("Qh07WQJsrKk032t80mw6nLkmczGPi9HwnpZbClojoq8S8djFjZh83YWoqmN9oMin",enc,{
+      keySize: tamaño/32,
+      iterations: 100
+    });
+
+    var iv = CryptoJS.lib.WordArray.random(128/8);
+    var encripted = CryptoJS.AES.encrypt(dato,Key,{
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+    });
+
+    var resultado = CryptoJS.enc.Base64.stringify(enc.concat(iv).concat(encripted.ciphertext));
+    return resultado;
+  }
+
+  obtenerIDunico(){
+    var navegador_info = window.navigator;
+    var pantalla_info = window.screen;
+    var vid:any = navegador_info.mimeTypes.length;
+    vid += navegador_info.userAgent.replace(/\D+/g,'');
+    vid += navegador_info.plugins.length;
+    vid += pantalla_info.height || '';
+    vid += pantalla_info.width || '';
+    vid += pantalla_info.pixelDepth || '';
+    return vid;
+  }
+
+  DesencriptarData(dato:any){
+    try {
+      const bytes = CryptoJS.AES.decrypt(dato,this.obtenerIDunico());
+      if(bytes.toString()){
+        return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      }else {
+        Swal.fire({
+          title:'Error',
+          text: 'Ha ocurrido un error',
+          icon: 'error'
+        });
+        return false;
+      }
+    } catch (error) {
+      Swal.fire({
+        title:'Error',
+        text: error,
+        icon: 'error'
+      });
+      return false;
+    }
+  }
+
+  EncriptarData(dato:any){
+    try {
+      return CryptoJS.AES.encrypt(JSON.stringify(dato), this.obtenerIDunico()).toString();
+    } catch (error) {
+      Swal.fire({
+        title:'Error',
+        text: error,
+        icon: 'error'
+      });
+      return false;
+    }
+  }
   logout(btn = 0){
     if(this.entity.Key != 0){
      if(btn == 0){
